6a0db95d3ce7e3a547fe10523d857644e1ba1762
zzf123zzf <1939536970@qq.com>
2025-09-18 18:56:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# 在这个文件里编写代码
+initial_weight = float(input())
+for year in range(1, 11):
+    earth_weight = initial_weight + 0.5 * year
+    moon_weight = earth_weight * 0.165
+    print(f"{year} {earth_weight:.1f} {moon_weight:.3f}")# 在这个文件里编写代码
